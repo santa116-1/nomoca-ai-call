7c4b9f9fba7bc8694f922958912c47d24bdf37e2
santa116-1 <totoplife116@gmail.com>
2024-07-19 02:41:27 +0900

    Fix Dashbord Backend

--- backend/backend/seo_article/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeoArticleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seo_article'
-    def ready(self):
-        import seo_article.signals
--- backend/backend/seo_article/scripts/init_user_profiles.py
+++
@@ -1,5 +0,0 @@
-from backend.users.models import User
-from seo_article.models import UserProfile
-
-for user in User.objects.all():
-    UserProfile.objects.get_or_create(user=user)
